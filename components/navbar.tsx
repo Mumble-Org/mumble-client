1a350628f9e2fd36d85f2dca182370f10a387d49
Teepee78 <eniolatoluwalase78@gmail.com>
2023-04-06 16:55:01 +0100

    Create navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./navbar.module.css";
+
+export function NavBar(props) {
+	return (
+		<div className={styles.container}>
+			<Link href="/">
+				<Image width="56" height="56" alt="mumble logo" src="/Logo.svg" />
+			</Link>
+
+			<div className={styles.search_container}>
+				<Image
+					width="17"
+					height="17"
+					alt="Search for producers, beats, genres and engineers"
+					src="/search.svg"
+				/>
+
+				<input placeholder="Search for producers, beats, genres and engineers"></input>
+			</div>
+
+			{props.loggedIn ? (
+				<div>logged in</div>
+			) : (
+				<div className={styles.actions_container}>
+					<div className={styles.login_signup}>
+						<Link href="/signup">Sign up</Link>
+						<p>/</p>
+						<Link href="/login">Login</Link>
+					</div>
+
+					<Link href="" className={styles.upload_beat}>
+						<Image
+							width="20"
+							height="20"
+							alt="upload a beat"
+							src="/upload_black.svg"
+						/>
+						<p>Upload A Beat</p>
+					</Link>
+				</div>
+			)}
+		</div>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+
+import { NavBar } from '../components/navbar';
 
 export default function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Mumble</title>
         <link rel="" href="" />
       </Head>
+
+      <NavBar loggedIn={loggedIn} />
     </div>
   )
 }
