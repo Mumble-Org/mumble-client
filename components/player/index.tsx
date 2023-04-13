0d5a36d63ef856fc573351cb53d78a567e0f57e8
codestronomer <johnrumide6@gmail.com>
2023-04-13 09:39:32 +0100

    [fix] player component styles

--- components/player/index.tsx
+++ components/player/index.tsx
@@ -1,24 +1,23 @@
 import React, { useEffect, useState } from "react";
 import ReactAudioPlayer from 'react-audio-player';
+import styles from './player.module.css';
 
 
 export function Player(props) {
 
-        return (
-                <div className="component">
-                        <img 
-                                className="musicCover"
-                                src={props.image}
-                        />
-                        <div>
-                                <h3 className="title">{props.title}</h3>
-                                <p className="subTitle">{props.artist}</p>
-                        </div>
-                        <div>
-                                <ReactAudioPlayer src={props.audioUrl} controls />
-                        </div>
-                </div>
-        );
-}
-
-
+    return (
+        <div className={styles.component}>
+						<img 
+								className={styles.musicCover}
+								src={props.image}
+						/>
+						<div>
+								<h3 className={styles.title}>{props.title}</h3>
+								<p className={styles.subTitle}>{props.artist}</p>
+						</div>
+						<div>
+								<ReactAudioPlayer src={props.audioUrl} controls />
+						</div>
+        </div>
+    );
+};
