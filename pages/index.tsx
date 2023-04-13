e9c478a7abeccac90ce48912790bf4a5da94535b
Teepee78 <eniolatoluwalase78@gmail.com>
2023-04-13 01:14:43 +0100

    Create trending beats header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,30 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css'
-import { useState } from 'react';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
-import { NavBar } from '../components/navigation/navbar';
-import { SubNav } from '../components/navigation/subnav';
-import { Player } from '../components/player';
+import { NavBar } from "../components/navigation/navbar";
+import { SubNav } from "../components/navigation/subnav";
+import { TrendingBeatsHome } from "../components/home/trending";
+import { Player } from "../components/player";
 
 export default function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
+	const [loggedIn, setLoggedIn] = useState(false);
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Mumble</title>
-        <link rel="" href="" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Mumble</title>
+				<link rel="" href="" />
+			</Head>
 
-      <NavBar loggedIn={loggedIn} />
-      <SubNav loggedIn={loggedIn} />
+			<NavBar loggedIn={loggedIn} />
+			<SubNav loggedIn={loggedIn} />
 
-      <div className={styles.player}>
-        <Player />
-      </div>
-    </div>
-  )
+			<TrendingBeatsHome />
+
+			<div className={styles.player}>
+				<Player />
+			</div>
+		</div>
+	);
 }
