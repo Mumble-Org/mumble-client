dd1ff5d022088911791ce0f8238fcb5da0dfcdf9
Teepee78 <eniolatoluwalase78@gmail.com>
2023-06-23 12:26:43 +0100

    [CHORE] Update api route in backend

--- utils/backend.ts
+++ utils/backend.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-
 export const backend = axios.create({
-	baseURL: 'https://mumble-core.vercel.app',
+	baseURL: 'https://api.mumble.com.ng',
 	headers: {
 		"Content-Type": "application/json"
 	}
